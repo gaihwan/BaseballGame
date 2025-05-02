dc9ee00f05b393e9006c01ce852af6e3999a0010
Janghwan.K <gaihwan@gmail.com>
2025-05-02 16:44:33 +0900

    question implemeted

--- TDD_BaseballGame/baseball.cpp
+++ TDD_BaseballGame/baseball.cpp
@@ -1,9 +1,24 @@
 #include <stdexcept>
 using namespace std;
 
+struct GuessResult {
+    bool solved;
+    int strikes;
+    int balls;
+};
 class Baseball {
 public:
-    void guess(const string& guessNumber) {
+    explicit Baseball(const string& question) : question{ question } {
+
+    }
+    GuessResult guess(const string& guessNumber) {
+        assertIllegalArgument(guessNumber);
+        return { true, 3, 0 };
+
+    }
+
+    void assertIllegalArgument(const std::string& guessNumber)
+    {
         if (guessNumber.length() != 3)
             throw length_error("Mst be tree letters");
 
@@ -11,12 +26,17 @@ public:
             if (ch < '0' || ch > '9')
                 throw invalid_argument("Must be number");
         }
-        if (guessNumber[0] == guessNumber[1] ||
-            guessNumber[0] == guessNumber[2] ||
-            guessNumber[1] == guessNumber[2]) {
+        if (isDuplicatedNumber(guessNumber)) {
             throw invalid_argument("Must not have the same number");
         }
-
     }
 
+    bool isDuplicatedNumber(const std::string& guessNumber)
+    {
+        return guessNumber[0] == guessNumber[1] ||
+            guessNumber[0] == guessNumber[2] ||
+            guessNumber[1] == guessNumber[2];
+    }
+private:
+    string question;
 };
